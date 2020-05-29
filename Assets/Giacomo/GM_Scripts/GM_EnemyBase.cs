23877a47bd27ac7da73f61c8c5c3dee74170e38e
GiacomoMazza <postagiacomo@gmail.com>
2020-05-29 18:22:08 +0100

    Update GM_EnemyBase.cs

--- Assets/Giacomo/GM_Scripts/GM_EnemyBase.cs
+++ Assets/Giacomo/GM_Scripts/GM_EnemyBase.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 public class GM_EnemyBase : MonoBehaviour
 {
     [SerializeField]
+    private GameObject go_Player;
+
+    [SerializeField]
     private EnemyType enemy_type;
+
+    private Logic enemy_stance;
+
+    [Tooltip("Toggle this to true ONLY if picowind is the only way this object can be triggered.")]
+    [SerializeField]
+    private bool bl_IsWindTheOnlyTrigger = false;
+
+    [Tooltip("Check if this is a ranged enemy. Uncheck if it is melee.")]
+    [SerializeField]
+    private bool bl_Ranged = false;
+
+    //ENEMY STATS
+    private float fl_ActivationRange = 0f;
 
     void Start()
     {
-        
+        enemy_stance = Logic.Inactive;
     }
 
     [SerializeField]
@@ -17,7 +33,15 @@ public class GM_EnemyBase : MonoBehaviour
     {
         Big_TV,
         Small_Bin,
-        Big_Bin,
+        Big_Bin
+    };
+
+    private enum Logic
+    {
+        Inactive,
+        Sleeping,
+        Attack,
+        Retreat
     };
 
     void Update()
@@ -25,14 +49,17 @@ public class GM_EnemyBase : MonoBehaviour
         switch (enemy_type)
         {
             case EnemyType.Big_Bin:
+                fl_ActivationRange = 10f;
                 Debug.Log("This is a big bin");
                 break;
 
             case EnemyType.Small_Bin:
+                fl_ActivationRange = 5f;
                 Debug.Log("This is a small bin");
                 break;
 
             case EnemyType.Big_TV:
+                fl_ActivationRange = 15f;
                 Debug.Log("This is a big tv");
                 break;
 
@@ -40,5 +67,48 @@ public class GM_EnemyBase : MonoBehaviour
                 Debug.Log("This is a bug");
                 break;
         }
+    }
+
+    private void LateUpdate()
+    {
+        //Calculate distance over time and activate object when the distance is equal or less than the range given.
+        if(Vector3.Distance(gameObject.transform.position, go_Player.transform.position) <= fl_ActivationRange)
+        {
+            if (!bl_IsWindTheOnlyTrigger)
+            {
+                RangedActivation(); 
+            }
+        }
+    }
+
+    /// <summary>
+    /// Run this function to activate the enemy with PicoWind, provide time.
+    /// </summary>
+    public void PicoWind(float fl_TimeBeforeActivation)
+    {
+        StartCoroutine("PicoAnimation", fl_TimeBeforeActivation);
+    }
+
+    /// <summary>
+    /// Pico-Animation based on the distance between the player and the enemy.
+    /// </summary>
+    private void RangedActivation()
+    {
+        if (bl_Ranged)
+        {
+            enemy_stance = Logic.Sleeping;
+        }
+
+        else if (!bl_Ranged)
+        {
+            enemy_stance = Logic.Attack;
+        }
+    }
+
+    private IEnumerator PicoAnimation(float fl_Time)
+    {
+        yield return new WaitForSeconds(fl_Time);
+        RangedActivation();
+        yield return null;
     }
 }
